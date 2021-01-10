e9fc1af6fb862e3929bffab3cf952b3781465bac
Nuno Nogueira <nguterresn@gmail.com>
2021-01-10 16:48:13 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 const char* ssid = "Quarto Escuro";
-const char* password = "nunorei2019";
+const char* password = "#";
 
 String ConfigWifi() {
 
